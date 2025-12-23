334a701acb51166628cf4411b23a9892c45b6a82
Abhay <abhaychilka18@gmail.com>
2025-12-24 01:55:21 +0530

    Assignment2

--- Assignment_2/Task_4.c
+++ Assignment_2/Task_4.c
@@ -17,11 +17,10 @@ int main()
 {
     DIR *dp = NULL;       //for open dir
     char dname[50];
-
     struct dirent *ptr = NULL; //read dir
-
     struct stat sobj;       //for stat of dir
     int iRet = 0;
+    char path[100];
 
     printf("Enter directory name : ");
     scanf("%s", dname);
@@ -45,7 +44,47 @@ int main()
         {
             continue;
         }
-            
+        
+        snprintf(path, sizeof(path), "%s/%s",dname, ptr->d_name); //IMP lstat requires full path not only name so it will make full path name 
+
+        printf("%s : File type is -", ptr->d_name);
+
+        iRet = lstat(path, &sobj);
+
+        if(iRet == -1)
+        {
+            perror("Unable to describe lstat");
+        }
+
+        if(S_ISBLK(sobj.st_mode))
+        {
+            printf("Block Device\n");
+        }
+        else if(S_ISCHR(sobj.st_mode))
+        {
+            printf("Character Device\n");
+        }
+        else if(S_ISDIR(sobj.st_mode))
+        {
+            printf("Directory File\n");
+        }
+        else if (S_ISREG(sobj.st_mode))
+        {
+            printf("Regular File\n");
+        }
+        else if (S_ISSOCK(sobj.st_mode))
+        {
+            printf("Socket File\n");
+        }
+        else if(S_ISFIFO(sobj.st_mode))
+        {
+            printf("Pipe File\n");
+        }
+        else if(S_ISLNK(sobj.st_mode))
+        {
+            printf("Symbolic File\n");
+        }
+
     }
 
     closedir(dp);
